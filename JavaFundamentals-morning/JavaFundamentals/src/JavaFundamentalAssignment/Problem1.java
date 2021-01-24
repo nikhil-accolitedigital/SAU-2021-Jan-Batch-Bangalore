6e9291abe160ec6ebf79f24d0485aad00ed582c3
nikhil-accolitedigital <77060470+nikhil-accolitedigital@users.noreply.github.com>
2021-01-24 18:32:22 +0530

    Java Fundamental Assignment

---
+++ JavaFundamentals-morning/JavaFundamentals/src/JavaFundamentalAssignment/Problem1.java
@@ -0,0 +1,26 @@
+package JavaFundamentalAssignment;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+
+public class Problem1 {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> arr= new ArrayList<Integer>();
+		arr.add(3);
+		arr.add(1);
+		arr.add(4);
+		arr.add(2);
+		arr.add(5);
+		
+		System.out.println("Unsorted Numbers in List: "+arr);
+		
+		Collections.sort(arr);
+		
+		System.out.println("Sorted Numbers in List: "+arr);
+		
+	}
+
+}
