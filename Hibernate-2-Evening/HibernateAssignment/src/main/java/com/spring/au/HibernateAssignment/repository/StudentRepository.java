2a57afb9b271a6d4dd89128d7389362946cb502b
nikhil.shrivastava@accolitedigital.com <nikhilshrivastava175@gmail.com>
2021-01-25 17:59:46 +0530

    Hibernate-2 Assignemnt

---
+++ Hibernate-2-Evening/HibernateAssignment/src/main/java/com/spring/au/HibernateAssignment/repository/StudentRepository.java
@@ -0,0 +1,43 @@
+package com.spring.au.HibernateAssignment.repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import com.spring.au.HibernateAssignment.entity.Student;
+
+
+
+
+@Repository
+@Transactional
+public class StudentRepository {
+	
+	@PersistenceContext
+	EntityManager entityManager;
+
+	
+
+	public void add(Student student) {
+		entityManager.persist(student);
+	}
+	
+	public void deleteById(int id) {
+		entityManager.remove(entityManager.find(Student.class,id));
+	}
+	
+	public Student update(Student student) {
+		return entityManager.merge(student);
+	}
+	
+	public Student getById(int id) {
+		return entityManager.find(Student.class,id);
+	}
+	
+	public Student getByName(String name) {
+		return entityManager.find(Student.class,name);
+	}
+
+}
