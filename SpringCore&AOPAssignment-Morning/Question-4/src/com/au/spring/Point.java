61b0de9a9359a9a6b5f4b1d4a731f295ddbbe4b3
nikhil-accolitedigital <77060470+nikhil-accolitedigital@users.noreply.github.com>
2021-01-30 13:28:01 +0530

    Update Point.java

--- SpringCore&AOPAssignment-Morning/Question-4/src/com/au/spring/Point.java
+++ SpringCore&AOPAssignment-Morning/Question-4/src/com/au/spring/Point.java
@@ -5,6 +5,12 @@ public class Point {
 	private int x;
 	private int y;
 	
+	Point(int x,int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
 	public int getX() {
 		return x;
 	}
@@ -17,5 +23,4 @@ public class Point {
 	public void setY(int y) {
 		this.y = y;
 	}
-
 }
