49024d7d4e5dc006cbf06505f337df5b6e89c63c
nikhil-accolitedigital <77060470+nikhil-accolitedigital@users.noreply.github.com>
2021-01-30 13:28:26 +0530

    Update Rectangle.java

--- SpringCore&AOPAssignment-Morning/Question-4/src/com/au/spring/Rectangle.java
+++ SpringCore&AOPAssignment-Morning/Question-4/src/com/au/spring/Rectangle.java
@@ -2,7 +2,12 @@ package com.au.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
+
+@Component
+@Scope(value="prototype")
 public class Rectangle {
 	
 	@Autowired
